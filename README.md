72976263bc98fdf6b5e97109cb18f5be6889488d
ikelab <ikelab@Abiko-no-Mac-Pro.local>
2019-10-11 11:52:42 +0900

    add new file

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # BdCNN
 Blind Denoising of Mixed Gaussian-impulse Noise by Single CNN
+
+Author: 
+
+Ryo Abiko, M.E. student of Electrical Engineering, Keio University, Japan.
+
+Masaaki Ikehara, Professor of Electrical Engineering, Keio University, Japan.
+
+
+The code is associated with the following paper:
+
+Ryo Abiko, and Masaaki Ikehara. "Blind Denoising of Mixed Gaussian-impulse Noise by Single CNN." ICASSP 2019-2019 IEEE International Conference on Acoustics, Speech and Signal Processing (ICASSP). IEEE, 2019.
+
+
+@inproceedings{abiko2019blind,
+  title={Blind Denoising of Mixed Gaussian-impulse Noise by Single CNN},
+  author={Abiko, Ryo and Ikehara, Masaaki},
+  booktitle={ICASSP 2019-2019 IEEE International Conference on Acoustics, Speech and Signal Processing (ICASSP)},
+  pages={1717--1721},
+  year={2019},
+  organization={IEEE}
+}
+
+
+Please cite this paper if you use this code. 
+For further information, please contact: {abiko, ikehara}@tkhm.elec.keio.ac.jp
+
+GPU devices are recommended.
---
+++ demo.m
@@ -0,0 +1,56 @@
+%{
+Please cite the following paper if you use this code:
+
+Ryo Abiko, and Masaaki Ikehara. 
+"Blind Denoising of Mixed Gaussian-impulse Noise by Single CNN." 
+ICASSP 2019-2019 IEEE International Conference on Acoustics, 
+Speech and Signal Processing (ICASSP). IEEE, 2019.
+
+denoised_image = trained_net.activations(noisy_image, trained_net.Layers(end-1).Name);
+
+gaussian_noise_sigma  : noise level of AWGN
+impulse_noise_rate    : noise level of RVIN
+sap_noise_rate        : noise level of SPIN
+%--------------------------------------------------------------------------
+%}
+
+clear all; close all;
+load BdCNN
+
+%% set parameters
+
+gaussian_noise_sigma = 15;
+impulse_noise_rate = 20;
+sap_noise_rate = 5;
+
+%% make noisy image
+image = im2single(imread('barbara.png'));
+noisy_image = image;
+% gaussian
+noisy_image = noisy_image + (gaussian_noise_sigma/255) * randn(size(image));
+% impulse
+img_noise_position = rand(size(image)) < impulse_noise_rate / 100;
+noisy_image(img_noise_position) = rand(1, sum(img_noise_position(:)));
+% salt and pepper
+img_noise_position = rand(size(image)) < sap_noise_rate / 100;
+noisy_image(img_noise_position) = rand(1, sum(img_noise_position(:)))>0.5;
+
+%% denoise
+
+denoised_image = trained_net.activations(noisy_image, trained_net.Layers(end-1).Name);
+
+
+figure(1)
+subplot(131)
+imshow(image)
+title('clean image')
+subplot(132)
+imshow(noisy_image)
+title('noisy image')
+subplot(133)
+imshow(denoised_image)
+title('denoised image')
+
+imwrite(noisy_image,'noisy_image.png')
+imwrite(denoised_image,'denoised_image.png')
+
